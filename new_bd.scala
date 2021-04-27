8a4e73a285390bcded3fe318b1afc16216bda280
Xiaochen Zong <ahbbzong@Xiaochens-MacBook-Pro.local>
2021-04-27 16:10:48 -0400

    revise K-means model

--- new_bd.scala
+++ new_bd.scala
@@ -1,15 +1,15 @@
-import org.apache.spark.mllib.clustering.{KMeans, KMeansModel};
-import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.linalg.Vectors
 
 // Load and parse the data
-val data = sc.textFile("data.txt");
-val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache();
+val data = sc.textFile("DataBig/data.txt")
+val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
 
 // Cluster the data into two classes using KMeans
-val numClusters = 3;
-val numIterations = 100;
-val clusters = KMeans.train(parsedData, numClusters, numIterations);
+val numClusters = 3
+val numIterations = 100
+val clusters = KMeans.train(parsedData, numClusters, numIterations)
 
 // Evaluate clustering by computing Within Set Sum of Squared Errors
-val WSSSE = clusters.computeCost(parsedData);
-println(s"Within Set Sum of Squared Errors = $WSSSE");
+val WSSSE = clusters.computeCost(parsedData)
+println("Within Set Sum of Squared Errors = " + WSSSE)
