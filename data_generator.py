166dfc3993dea45a29381bd59e28320576ed977b
Yiwei Chen <reed@Yiweis-MacBook-Air.local>
2021-04-26 21:18:18 -0400

    changes

--- data_generator.py
+++ data_generator.py
@@ -22,6 +22,7 @@ def Printlsvm2(label,R,D):
     print("")
 
 
+res = []
 for i in range(N):
     R[i][3] = 0 # set the empty dimesion
     x = R[i][0]
@@ -29,8 +30,16 @@ for i in range(N):
     z = R[i][2]
     if z > ((x*x+y*y)/5.0)-2.0:
         Printlsvm(2.0,R[i],D)
-    elif z > ((x*x+y*y)/5.0)-7.0:
+        res.append(2)
+    elif z > ((x*x+y*y)/5.0)-5.0:
         Printlsvm(1.0,R[i],D)
+        res.append(1)
+
     else:
         Printlsvm(0.0,R[i],D)
+        res.append(0)
 
+
+print("2:" + str(res.count(2)))
+print("1:" + str(res.count(1)))
+print("0:" + str(res.count(0)))
