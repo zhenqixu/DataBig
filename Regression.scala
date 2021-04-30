86cd84d4e9a600ba7eb61ea924a6c4714275ba2e
Xiaochen Zong <ahbbzong@Xiaochens-MacBook-Pro.local>
2021-04-30 14:14:02 -0400

    Make it able to get rid of inf, null data

--- Regression.scala
+++ Regression.scala
@@ -1,15 +1,12 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.rdd.{PairRDDFunctions,RDD}
 import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.sql.{SQLContext}
 
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils;
-import org.apache.spark.mllib.classification.SVMWithSGD;
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.evaluation.MulticlassMetrics;
@@ -18,11 +15,12 @@ import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticR
 
 val sqlCtx = new SQLContext(sc)
 import sqlCtx._
-val data_1 = sqlCtx.jsonFile("data.json")
+val data_1 = sqlCtx.jsonFile("DataBig/data.json")
 data_1.show
-val RDD = data_1.rdd.map(_.mkString(" ")).map(line=>line.split(" ").filter(str=>(str!="null" && str!="Infinity")).map(_.toDouble))
+val size = data_1.columns.size
+val RDD = data_1.rdd.map(_.mkString(" ")).map(line=>line.split(" ").filter(str=>str.exists(_.isLetter)^true).map(_.toDouble)).filter(s=>s.length==size)
+RDD.map(_.mkString(" ")).collect().foreach(println)
 val data = RDD.map(s=>LabeledPoint(s(4), Vectors.dense(s(0),s(1),s(2),s(3))))
-data.take(3).foreach(println)
 
 // Split data into training (60%) and test (40%).
 val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
