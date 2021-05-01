eac2cbf32f71bceae941013ef0cf6e63d07b6f6c
Xiaochen Zong <ahbbzong@gmail.com>
2021-05-01 12:46:39 -0400

    add normalizer

--- Regression.scala
+++ Regression.scala
@@ -13,6 +13,8 @@ import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.evaluation.MulticlassMetrics;
 import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
 import org.apache.spark.mllib.feature.PCA
+import org.apache.spark.mllib.feature.Normalizer
+import org.apache.spark.mllib.util.MLUtils
 
 import org.apache.spark.rdd.RDD
 
@@ -23,15 +25,21 @@ data_1.show
 val size = data_1.columns.size
 val RDD = data_1.rdd.map(_.mkString(" ")).map(line=>line.split(" ").filter(str=>str.exists(_.isLetter)^true).map(_.toDouble)).filter(s=>s.length==size)
 //RDD.map(_.mkString(" ")).collect().foreach(println)
+
 val data = RDD.map(s=>LabeledPoint(s(4), Vectors.dense(s(0),s(1),s(2),s(3))))
 data.take(3).foreach(println)
 
-val pca = new PCA(2).fit(data.map(_.features))
-val projected = data.map(p => p.copy(features = pca.transform(p.features)))
+val normalizer1 = new Normalizer()
+val data1 = data.map(x => LabeledPoint(x.label, normalizer1.transform(x.features)))
+data1.take(3).foreach(println)
+
+
+val pca = new PCA(2).fit(data1.map(_.features))
+val projected = data1.map(p => p.copy(features = pca.transform(p.features)))
 projected.take(5).foreach(println)
 
-// Split data into training (60%) and test (40%).
-val splits = projected.randomSplit(Array(0.6, 0.4), seed = 11L)
+// Split data into training (80%) and test (20%).
+val splits = projected.randomSplit(Array(0.8, 0.2), seed = 11L)
 val training = splits(0).cache()
 val test = splits(1)
 
